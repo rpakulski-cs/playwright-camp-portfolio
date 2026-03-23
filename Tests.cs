77a1e26d1d2eec4d351d66efe99294640d7d583b
rpakulski-cs <radoslaw.pakulski1@gmail.com>
2026-03-23 16:32:24 +0100

    first test

---
+++ Tests.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace playwright_camp_portfolio;
+
+public class Tests : PageTest
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        var page = new SearchPage(Page);
+        await page.Navigate();
+        await page.FillUsername("Dzialam!!!!");
+
+        await page.ClickLogin();
+
+        Assert.IsTrue(Page.Url.Contains("saucedemo"));
+    }
+}
