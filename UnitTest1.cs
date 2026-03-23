7d6b9d2296fb6b5426f97378aa2dc4ae25e0a2e6
rpakulski-cs <radoslaw.pakulski1@gmail.com>
2026-03-23 16:07:15 +0100

    project sceleton

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace playwright_camp_portfolio;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
